8782da151c8e964578d054383eef9c33615639c5
Ritvik <44639543+chirothespearow@users.noreply.github.com>
2022-08-01 23:29:13 +0530

    Update test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -24,12 +24,12 @@ async def test_seq_bug1(dut):
     await Timer(12, units='us')
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
-    sequence=[1,0,1,1,1,0,1,1,0,1,1,1,0,1,1]
-    ou=      [0,0,0,1,0,0,0,1,0,0,1,0,0,0,1]
+    sequence=[1,0,1,1,1,0,1,1,0,1,1,1,0,1,1]                #Input Sequence
+    ou=      [0,0,0,1,0,0,0,1,0,0,1,0,0,0,1]                #Expected output
     count=0
     print('Input:\n101110110111011\n')
     print("Output: ")
-    for i in sequence:
+    for i in sequence:                                      # Traversing through the sequence and comparing model output with expected output
         dut.inp_bit.value = i 
         #await RisingEdge(dut.clk)
         await Timer(10, units='us')
@@ -38,7 +38,7 @@ async def test_seq_bug1(dut):
         print(dut.seq_seen.value, end= '')
     print('\n')
     
-
+#Similiar to above but using a random sequence
 @cocotb.test()
 async def test_seq_bug1_random(dut):
     """Test for seq detection """
@@ -55,9 +55,11 @@ async def test_seq_bug1_random(dut):
     await Timer(12, units='us')
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    # Code to detect sequence in python
     sequence=[randint(0,1) for x in range(15)]
     stack=[]
     ou=      []
+    #When the stack contains the sequence 1011, an output 1 is broadcasted
     for iterat in sequence:
         if len(stack)<4:
             stack.append(iterat)
